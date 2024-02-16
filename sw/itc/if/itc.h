ae201d0cb12b044408c54edd9ad422622c2bf678
Giang Nguyen <giang.nguyentbk@gmail.com>
2024-02-17 01:42:44 +0700

    NOREF: Add delares for itci_send and itci_receive
    Team: GreenZone
    giangnguyentbk

---
+++ sw/itc/if/itc.h
@@ -0,0 +1,31 @@
+/*
+* _______________________   __________                                                    ______  
+* ____  _/__  __/_  ____/   ___  ____/____________ _______ ___________      _________________  /__
+*  __  / __  /  _  /        __  /_   __  ___/  __ `/_  __ `__ \  _ \_ | /| / /  __ \_  ___/_  //_/
+* __/ /  _  /   / /___      _  __/   _  /   / /_/ /_  / / / / /  __/_ |/ |/ // /_/ /  /   _  ,<   
+* /___/  /_/    \____/      /_/      /_/    \__,_/ /_/ /_/ /_/\___/____/|__/ \____//_/    /_/|_|  
+*                                                                                                 
+*/
+
+
+// Okay, first let's create an itc API declarations. Which functions we will offer to the end users.
+
+#ifndef __ITC_H__
+#define __ITC_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+
+
+#define ITC_NAME_MAXLEN     255
+
+typedef uint32_t itc_mbox_id_t;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __ITC_H__
